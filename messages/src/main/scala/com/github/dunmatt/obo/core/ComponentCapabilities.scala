d5f6be6e1781a02e9fcbc4849da92c14f3d5a8b6
M@ Dunlap <matthew.dunlap@gmail.com>
2017-01-02 16:31:12 -0500

    made ComponentCapabilities serialize and deserialize the set of topics

--- messages/src/main/scala/com/github/dunmatt/obo/core/ComponentCapabilities.scala
+++ messages/src/main/scala/com/github/dunmatt/obo/core/ComponentCapabilities.scala
@@ -1,24 +1,23 @@
 package com.github.dunmatt.obo.core
 
-import msgpack.{ MsgBuilder, MsgReader }
+import msgpack.{ MsgBuilder, MsgReader, StringField }
 import scala.util.Try
 
-// TODO: put actual information here
 case class ComponentCapabilities(topics: Set[RuntimeResourceName]) extends Message[ComponentCapabilities] {
   type Factory = ComponentCapabilitiesFactory
   def factory = classOf[ComponentCapabilitiesFactory]
 
-  // TODO: put topics, once collections are supported by msgpack
-  def getBytes: Array[Byte] = (new MsgBuilder).putString("factoryClassName").getBytes
+  def getBytes: Array[Byte] = {
+    (new MsgBuilder).putSeq(topics.map(t => new StringField(t.name)).toSeq).getBytes
+  }
 }
 
 class ComponentCapabilitiesFactory extends MessageFactory[ComponentCapabilities] {
   def unpack(rdr: MsgReader): Try[ComponentCapabilities] = {
     for {
-      // TODO: get topics, once collections are supported by msgpack
-      fcn <- rdr.getString(0)
+      ts <- rdr.getSeq(0, "")
     } yield {
-      ComponentCapabilities(Set.empty)
+      ComponentCapabilities(ts.map(t => RuntimeResourceName(t)).toSet)
     }
   }
 }
